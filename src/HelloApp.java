70f3050da35f18b56ca5f13072a6e50a4ff435cf
unknown <hs2419@srmist.edu.in>
2026-03-26 13:56:01 +0530

    UC-2

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,5 +1,10 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (args.length == 0) {
+            System.out.println("Hello, Shubhi Sharma");
+            return;
+        }
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }
